ebb2249e4eddef320aa0519914f226ee67318a47
Daniil Kovalev <kovdan01@yandex.ru>
2021-11-05 11:49:01 +0300

    Use a function table instead of value calculation
    It seems that this is more natural use scenario in terms of numeric integration

--- parallel-computing/task1/task1.2.cpp
+++ parallel-computing/task1/task1.2.cpp
@@ -1,64 +1,70 @@
 #include "tools.hpp"
 
 #include <cmath>
-#include <cstdint>
-#include <cstdlib>
-#include <cstring>
 #include <functional>
 #include <iomanip>
 #include <iostream>
-#include <limits>
 #include <numeric>
 #include <string>
 #include <string_view>
 #include <vector>
 
 using arithmetic_function_t = std::function<double(double)>;
+using function_values_table_t = std::vector<double>;
 
-struct IntegrateParams
-{
-    arithmetic_function_t f;
-    double dx;
-    double from;
-    double to;
-};
+using integrate_function_t = std::function<double(const function_values_table_t& table, double dx)>;
 
-using integrate_function_t = std::function<double(IntegrateParams)>;
-
-double integrate_dummy(IntegrateParams params)
+double integrate_dummy(const function_values_table_t& table, double dx)
 {
     asm volatile("# integrate_dummy enter");
     double sum = 0;
-    for (double x = params.from; x < params.to; x += params.dx)
+    for (double value : table)
     {
-        sum += params.dx * params.f(x);
+        sum += dx * value;
     }
     asm volatile("# integrate_dummy exit");
     return sum;
 }
 
-double integrate_omp(IntegrateParams params)
+double integrate_omp_parallel(const function_values_table_t& table, double dx)
 {
-    asm volatile("# integrate_omp enter");
+    asm volatile("# integrate_omp_parallel enter");
     double sum = 0;
-    std::size_t n = (params.to - params.from) / params.dx;
     #pragma omp parallel for reduction(+ : sum)
-    for (std::size_t i = 0; i != n; ++i)
+    for (std::size_t i = 0; i != table.size(); ++i)
     {
-        sum += params.dx * params.f(params.from + i * params.dx);
+        sum += dx * table[i];
     }
-    asm volatile("# integrate_omp exit");
+    asm volatile("# integrate_omp_parallel exit");
     return sum;
 }
 
-my::BenchmarkResult measure_integrate(integrate_function_t integrate, IntegrateParams params)
+double integrate_omp_simd(const function_values_table_t& table, double dx)
 {
-    static constexpr std::size_t ITERATIONS_COUNT = 10'000;
+    asm volatile("# integrate_omp_simd enter");
+    double sum = 0;
+    #pragma omp simd reduction(+:sum)
+    for (std::size_t i = 0; i != table.size(); ++i)
+    {
+        sum += dx * table[i];
+    }
+    asm volatile("# integrate_omp_simd exit");
+    return sum;
+}
+
+double arithmetic_function(double x)
+{
+    return std::exp(std::sin(std::pow(x, M_PI)));
+};
+
+my::BenchmarkResult measure_integrate(integrate_function_t integrate, const function_values_table_t& table, double dx)
+{
+    static constexpr std::size_t ITERATIONS_COUNT = 10'000'000;
     std::vector<my::BenchmarkResult> results(ITERATIONS_COUNT);
     for (my::BenchmarkResult& result : results)
     {
         my::Timer timer(result);
-        double integrate_result = integrate(params);
+        double integrate_result = integrate(table, dx);
         my::do_not_optimize(integrate_result);
     }
     my::BenchmarkResult results_sum = std::accumulate(results.begin(), results.end(), my::BenchmarkResult{ 0, 0 },
@@ -76,31 +82,35 @@ void print_table_row(std::string_view label, my::BenchmarkResult result)
     std::cout << "| " << label << " | "
               << std::setw(14) << std::setprecision(2) << std::fixed << result.ticks << " | "
               << std::setw(13)  << std::setprecision(2) << std::fixed << result.nanoseconds << " |" << std::endl
-              << "+--------------------+----------------+---------------+" << std::endl;
+              << "+--------------------------+----------------+---------------+" << std::endl;
+}
+
+std::vector<double> generate_function_values_table(arithmetic_function_t f, double from, double to, double dx)
+{
+    std::vector<double> table;
+    table.reserve((to - from) / dx);
+    for (double x = from; x <= to; x += dx)
+    {
+        table.emplace_back(f(x));
+    }
+    return table;
 }
 
 int main() try
 {
-    auto arithmetic_function = [](double x)
-    {
-        return std::exp(std::sin(std::pow(x, M_PI)));
-    };
-    IntegrateParams params =
-    {
-        .f = arithmetic_function,
-        .dx = 0.0001,
-        .from = -43.54325,
-        .to = 34.6354
-    };
+    double dx = 0.0001;
+    function_values_table_t table = generate_function_values_table(arithmetic_function, -43.54325, 34.6354, dx);
 
-    my::BenchmarkResult integrate_dummy_result = measure_integrate(integrate_dummy, params);
-    my::BenchmarkResult integrate_omp_result = measure_integrate(integrate_omp, params);
+    my::BenchmarkResult integrate_dummy_result = measure_integrate(integrate_dummy, table, dx);
+    my::BenchmarkResult integrate_omp_parallel_result = measure_integrate(integrate_omp_parallel, table, dx);
+    my::BenchmarkResult integrate_omp_simd_result = measure_integrate(integrate_omp_simd, table, dx);
 
-    std::cout << "+--------------------+----------------+---------------+" << std::endl
-              << "|      operation     |  ticks / iter  |   ns / iter   |" << std::endl
-              << "+--------------------+----------------+---------------+" << std::endl;
-    print_table_row(" integrate dummy  ", integrate_dummy_result);
-    print_table_row("  integrate omp   ", integrate_omp_result);
+    std::cout << "+--------------------------+----------------+---------------+" << std::endl
+              << "|         operation        |  ticks / iter  |   ns / iter   |" << std::endl
+              << "+--------------------------+----------------+---------------+" << std::endl;
+    print_table_row("    integrate dummy     ", integrate_dummy_result);
+    print_table_row(" integrate omp parallel ", integrate_omp_parallel_result);
+    print_table_row("   integrate omp simd   ", integrate_omp_simd_result);
 
     return EXIT_SUCCESS;
 }
